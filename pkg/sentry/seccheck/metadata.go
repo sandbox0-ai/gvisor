c7dad8f073375e35eb3874c1464d0534ce2adf55
Xin Zhong <xinzhong@google.com>
2026-07-14 11:31:26 -0700

    Add seccheck support for collecting stdin/stdout/stderr info on execve.
    This change introduces a new seccheck field, FieldSentryExecveFdInfo, for the
    Execve trace point. When enabled, this field collects information about file
    descriptors 0, 1, and 2 (stdin, stdout, stderr) and populates the new stdin,
    stdout, and stderr fields in the ExecveInfo proto. The collected FdInfo
    includes the file path, mode, inode, whether it's on an overlayfs upper
    layer, and socket details (local/remote addresses) if the FD is a socket.
    
    PiperOrigin-RevId: 947808754

--- pkg/sentry/seccheck/metadata.go
+++ pkg/sentry/seccheck/metadata.go
@@ -66,6 +66,10 @@ const (
 	// FieldSentryExecveBinarySha256 is an optional field to collect the SHA-256
 	// hash of the binary being executed.
 	FieldSentryExecveBinarySha256
+
+	// FieldSentryExecveFdInfo is an optional field to collect information
+	// about standard file descriptors (stdin, stdout, stderr).
+	FieldSentryExecveFdInfo
 )
 
 // Points is a map with all the trace points registered in the system.
@@ -128,7 +132,7 @@ type SinkDesc struct {
 	// is called. config is an opaque json object passed to the sink.
 	Setup func(config map[string]any) (*os.File, error)
 	// New creates a new sink. config is an opaque json object passed to the sink.
-	// endpoing is a file descriptor to the file returned in Setup. It's set to -1
+	// endpoint is a file descriptor to the file returned in Setup. It's set to -1
 	// if Setup returned nil.
 	New func(config map[string]any, endpoint *fd.FD) (Sink, error)
 }
@@ -253,6 +257,10 @@ func genericInit() {
 			{
 				ID:   FieldSentryExecveBinarySha256,
 				Name: "binary_sha256",
+			},
+			{
+				ID:   FieldSentryExecveFdInfo,
+				Name: "fd_info",
 			},
 		},
 		ContextFields: defaultContextFields,
